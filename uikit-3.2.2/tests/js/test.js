68eaae873542be7149af4348148ad9cfd263543f
T. Kameda <kame@toiee.jp>
2019-11-06 17:43:20 +0900

    テスト用データを同期するように変更

---
+++ uikit-3.2.2/tests/js/test.js
@@ -0,0 +1,3 @@
+document.write('<link rel="stylesheet" href="../../site.css" type="text/css" />');
+document.write('<script src="https://cdn.jsdelivr.net/npm/uikit@3.2.2/dist/js/uikit.min.js"></script>');
+document.write('<script src="https://cdn.jsdelivr.net/npm/uikit@3.2.2/dist/js/uikit-icons.min.js"></script>');
